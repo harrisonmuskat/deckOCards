a09eb6bef9285d0dbef820bc3a45455bf0f5588f
Harrison Muskat <harrisonmuskat@gmail.com>
2018-06-28 22:26:26 -0400

    fix: https

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   	<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" rel="stylesheet">
 	</head>
 	<body>
